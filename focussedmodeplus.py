390c6b6edf24a5a7905ed74f98446c3bb37f1780
Prashanth Kamath <prashanth_kamath_2001@hotmail.com>
2022-11-27 21:06:28 +0530

    second commit......
    hardened the mouse move part
    trying to implement keyboard navigation - but facing some issue [new visible position resets to old position]......

--- focussedmodeplus.py
+++ focussedmodeplus.py
@@ -6,7 +6,7 @@ import win32gui
 pygame.init()
 scrres = pygame.display.Info()
 screen = pygame.display.set_mode((scrres.current_w, scrres.current_h),pygame.NOFRAME) # For borderless, use pygame.NOFRAME
-px, py, width, hight = 45, 45, 250, 200
+px, py, width, hight, horstepval, vertstepval = 45, 45, 250, 150, 150, 100
 fuchsia = (255, 0, 128)  # Transparency color
 black = (0, 0, 0)
 
@@ -19,25 +19,52 @@ win32gui.SetLayeredWindowAttributes(hwnd, win32api.RGB(*fuchsia), 0, win32con.LW
 
 def main():
     ndone = True
+    #px, py = 50, 50
     while ndone:
+        
+        #px, py = pygame.mouse.get_pos()
+        
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                ndone = False
-            #if event.type == pygame.MOUSEMOTION():
+            
             px, py = pygame.mouse.get_pos()
             
-            if px >= scrres.current_w - width:
-                px = scrres.current_w - width
-            if py >= scrres.current_h - hight:
-                py = scrres.current_h - hight
-        
-        
+            if event.type == pygame.QUIT:
+                ndone = False
+            
+            #if event.type == pygame.MOUSEMOTION:    
+            #    px, py = pygame.mouse.get_pos()
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    if px >= 0:
+                        px -= horstepval
+                    else:
+                        px = 0
+                if event.key == pygame.K_RIGHT:
+                    if px <= scrres.current_w:
+                        px += horstepval
+                    else:
+                        px = scrres.current_w
+                if event.key == pygame.K_UP:
+                    if py >= 0:
+                        py -= vertstepval
+                    else:
+                        py = 0
+                if event.key == pygame.K_DOWN:
+                    if py <= scrres.current_h:
+                        py += vertstepval
+                    else:
+                        py = scrres.current_h
+                
+                #pygame.mouse.set_pos(px, py)
+                #px, py = pygame.mouse.get_pos()
+                                
         screen.fill(black)
         # Transparent Rectangle
         drawtransparentrectangle(px, py)
         pygame.display.update()
         
 def drawtransparentrectangle(px, py):
-    pygame.draw.rect(screen, fuchsia, pygame.Rect(px-width, py-hight, width, hight))
+    pygame.draw.rect(screen, fuchsia, pygame.Rect(px-int(width/2), py-int(hight/2), width, hight))
     
 main()
